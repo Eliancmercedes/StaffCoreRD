42122a313a4eb3176e6208c54888cf254dcb644c
Elian <elianrd10@gmail.com>
2026-07-28 11:52:45 -0400

    StaffDbContext, Identity y roles automaticos configurados en Program.cs

--- StaffCoreRD/Program.cs
+++ StaffCoreRD/Program.cs
@@ -1,15 +1,47 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StaffCoreRD.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Servicios
+builder.Services.AddDbContext<StaffDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StaffCore")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+})
+.AddEntityFrameworkStores<StaffDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Crear roles automáticamente al iniciar
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    string[] roles = { "Administrador", "RRHH", "Viewer" };
+    foreach (var rol in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(rol))
+            await roleManager.CreateAsync(new IdentityRole(rol));
+    }
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,6 +50,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // SIEMPRE antes de UseAuthorization
 app.UseAuthorization();
 
 app.MapControllerRoute(

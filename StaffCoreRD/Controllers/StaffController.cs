404e09402fbef88ab40ec725e9a6f18d2f31244b
Elian <elianrd10@gmail.com>
2026-07-30 09:21:35 -0400

    StaffController Index con lista de personal activo y total de nomina

---
+++ StaffCoreRD/Controllers/StaffController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StaffCoreRD.Data;
+using StaffCoreRD.Models;
+
+namespace StaffCoreRD.Controllers
+{
+    [Authorize]
+    public class StaffController : Controller
+    {
+        private readonly StaffDbContext _context;
+
+        public StaffController(StaffDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Staff
+        [Authorize(Roles = "Administrador,RRHH,Viewer")]
+        public async Task<IActionResult> Index()
+        {
+            var personal = await _context.Personal
+                .Where(s => s.Activo)
+                .OrderBy(s => s.Nombre)
+                .ToListAsync();
+
+            return View(personal);
+        }
+    }
+}

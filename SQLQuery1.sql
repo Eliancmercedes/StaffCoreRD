5aa05d8e5753e32ce10d01bf5e6e613b29c788a4
Elian <elianrd10@gmail.com>
2026-08-02 21:51:33 -0400

    Verificacion completa de roles Administrador, RRHH y Viewer

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿DELETE FROM AspNetUserRoles
+WHERE UserId = 'd73522f3-d59c-4b89-bdc6-31ffdcef61fb'
+AND RoleId = (SELECT Id FROM AspNetRoles WHERE Name = 'Viewer');
+
+INSERT INTO AspNetUserRoles (UserId, RoleId)
+VALUES ('d73522f3-d59c-4b89-bdc6-31ffdcef61fb', (SELECT Id FROM AspNetRoles WHERE Name = 'RRHH'));
